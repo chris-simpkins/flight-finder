6936a02063160da868abea39f92bce58c5d8ae0e
chris-simpkins <c.simpkins5@gmail.com>
2022-05-06 22:43:58 +0100

    added typescript

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import axios from "axios";
+import * as credentials from "../credentials.json";
+
+const defaultCurrency = "GBP";
+
+const query = {
+  adults: "1",
+  origin: "LHR",
+  destination: "MCO",
+  departureDate: "2022-06-30"
+};
+
+const getFlights = async () => {
+  try {
+    const response = await axios.get(
+      `https://skyscanner44.p.rapidapi.com/search-extended`,
+      {
+        params: {
+          adults: query.adults,
+          origin: query.origin,
+          destination: query.destination,
+          departureDate: query.departureDate,
+          currency: defaultCurrency
+        },
+        headers: {
+          "X-RapidAPI-Host": credentials.rapidApi.host,
+          "X-RapidAPI-Key": credentials.rapidApi.apiKey
+        }
+      }
+    );
+    return response;
+  } catch (err) {
+    return err;
+  }
+};
+
+const response = await getFlights();
+
+console.log(response);
